cf08088811f35d7cf9bab5e6b41bbcc5c1296958
matteodb989 <matteodb989@gmail.com>
2023-02-28 12:16:07 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/cf1f95c9-1c45-4699-b596-2993469cb010/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
